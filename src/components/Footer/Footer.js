232fb1b071eb87c33d6a4f0aee8e399857a3a0be
Victor <victorcanasint@gmail.com>
2018-04-13 19:22:15 -0400

    click

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Footer.css";
+
+// Footer component
+const Footer = props => (
+    <div class="footer-copyright py-3 text-center text-white">
+        Click Game!
+    </div>
+);
+
+export default Footer;
